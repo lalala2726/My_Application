78dfb5f652d5a0b51a3c9fe54ece6303e970c0f5
Zhangchuang <3306056974@qq.com>
2023-03-07 16:53:06 +0800

    停车场s暂时完结！

---
+++ app/src/main/java/com/zhangchuang/demo/entity/Parking.java
@@ -0,0 +1,18 @@
+package com.zhangchuang.demo.entity;
+
+/**
+ * author ZhangChuang
+ * date 2023/3/7 15:29
+ * description:
+ */
+
+public class Parking {
+    private Integer id;
+    private String parkName;
+    private String vacancy;
+    private String priceCaps;
+    private String imgUrl;
+    private String rates;
+    private String address;
+    private String distance;
+}
--- app/src/main/java/com/zhangchuang/demo/ui/function/StopCarActivity.java
+++ app/src/main/java/com/zhangchuang/demo/ui/function/StopCarActivity.java
@@ -1,7 +1,6 @@
 package com.zhangchuang.demo.ui.function;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +8,9 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.zhangchuang.demo.R;
 import com.zhangchuang.demo.network.api.SystemService;
@@ -35,6 +37,20 @@ public class StopCarActivity extends AppCompatActivity {
 
     private ApplicationServiceImpl applicationService;
 
+    //原始JSON数据
+    private List<JSONObject> list;
+
+    //整理后的JSON数据
+    //停车场标题
+    private List<String> parkName;
+    //详情
+    private List<String> address;
+    //剩余车位
+    private List<String> vacancy;
+    //距离
+    private List<String> distance;
+    //价格
+    private List<String> rates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,24 +124,88 @@ public class StopCarActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * 将JSON集合转List集合
+     *
+     * @param json JSON集合
+     */
     private void finishingJson(String json) {
         try {
             JSONArray jsonArray = new JSONArray(json);
+            System.out.println("JSON转换的数据-->" + jsonArray);
+            //将JSON集合转换为List集合
+            list = new ArrayList<>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                list.add(jsonObject);
+            }
+            System.out.println("整理后的数据-->" + list);
+            sortingData();
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * 整理数据
+     */
+    public void sortingData() {
+        //将List集合里面的数据整理成List集合
+        parkName = new ArrayList<>();
+        address = new ArrayList<>();
+        vacancy = new ArrayList<>();
+        distance = new ArrayList<>();
+        rates = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            JSONObject jsonObject = list.get(i);
+            try {
+               /* String parkName = jsonObject.getString("parkName");
+                System.out.println("停车场名称-->" + parkName);
+                String address = jsonObject.getString("address");
+                System.out.println("详细信息-->" + address);
+                String vacancy = jsonObject.getString("vacancy");
+                System.out.println("剩余车位-->" + vacancy);
+                String distance = jsonObject.getString("distance");
+                System.out.println("距离-->" + distance);
+                String rates = jsonObject.getString("rates");
+                System.out.println("价格-->" + rates);*/
+                parkName.add(jsonObject.getString("parkName"));
+                address.add(jsonObject.getString("address"));
+                vacancy.add(jsonObject.getString("vacancy"));
+                distance.add(jsonObject.getString("distance"));
+                rates.add(jsonObject.getString("rates"));
+            }catch (JSONException e){
+                throw new RuntimeException(e);
+            }
+
+            //遍历集合数据
+            for (int j = 0; j < parkName.size(); j++) {
+                System.out.println("停车场名称-->" + parkName.get(j));
+                System.out.println("详细信息-->" + address.get(j));
+                System.out.println("剩余车位-->" + vacancy.get(j));
+                System.out.println("距离-->" + distance.get(j));
+                System.out.println("价格-->" + rates.get(j));
+            }
+        }
+
+        //将数据显示在ListView上
+        ListView listView = findViewById(R.id.car_ListView);
+        CarAdapter carAdapter = new CarAdapter();
+        listView.setAdapter(carAdapter);
+
+    }
+
+
     private class CarAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
-            return 0;
+            return parkName.size();
         }
 
         @Override
         public Object getItem(int position) {
-            return null;
+            return parkName.get(position);
         }
 
         @Override
@@ -135,8 +215,28 @@ public class StopCarActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
+            //将List集合的数据显示在TextView上
+            View view = View.inflate(getApplicationContext(),R.layout.car_layout,null);
+
+            //停车场标题
+            TextView title = view.findViewById(R.id.text_title);
+            title.setText(parkName.get(position));
+            //停车场详情
+            TextView details = view.findViewById(R.id.text_details);
+            details.setText(address.get(position));
+            //停车场价格
+            TextView amount = view.findViewById(R.id.text_amount);
+            amount.setText(rates.get(position));
+            //停车场剩余位置
+            TextView textRemaining = view.findViewById(R.id.text_remaining);
+            textRemaining.setText(vacancy.get(position));
+            //停车场距离
+            TextView textDistance = view.findViewById(R.id.text_distance);
+            textDistance.setText(distance.get(position));
+
+            return view;
         }
     }
+
 
 }
